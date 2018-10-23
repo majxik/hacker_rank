81d01c5a075097199fc6d1dc75bc005550c84f83
majxik <majxik@gmail.com>
2018-10-23 18:43:33 +0200

    Uncompleted problem_BirthdayChocolate
    method prepared, loop needs to be completed.

---
+++ problem_BirthdayChocolate.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+s = [2,2,1,3,2]
+d = 4
+m = 3
+
+
+#s: an array of integers, the numbers on each of the squares of chocolate
+#d: an integer, Ron's birth day
+#m: an integer, Ron's birth month (lenght)
+
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+    len_s = len(s)
+    index = 0
+    index1 = 0
+    while index != (len_s + 1 - m):
+        while index1 < m
+
+        index += 1
+
+
+birthday(s, d, m)
+
+input()
+
+#https://www.hackerrank.com/challenges/the-birthday-bar/problem
